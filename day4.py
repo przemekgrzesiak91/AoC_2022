7a5b4c3c0e03e7b08f58405329595ac9aa3398be
Przemek <przemek.grzesiak91@gmail.com>
2022-12-04 07:26:37 +0100

    Day 4 solution, both parts

---
+++ day4.py
@@ -0,0 +1,18 @@
+with open('data/day4.txt', 'r') as f:
+    data = f.read().splitlines()
+
+overlaps = 0
+overlaps2 = 0
+for row in data:
+    x1,y1,x2,y2 = [int(x) for x in row.replace('-',',').split(',')]
+
+    a = set(range(x1, y1+1))
+    b = set(range(x2, y2+1))
+
+    if a.issubset(b) or a.issuperset(b):
+        overlaps += 1
+    if a.intersection(b):
+        overlaps2 += 1
+
+print(overlaps)
+print(overlaps2)
